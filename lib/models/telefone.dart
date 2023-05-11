35723a1f58ffafbc70e960e695d6f6a1f0545ae0
Caio Guge <caioguge@gmail.com>
2023-05-11 00:06:43 -0300

    Introdução a Serialização e Desserialização de Modelos

--- lib/models/telefone.dart
+++ lib/models/telefone.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Telefone {
   int ddd;
   String numero;
@@ -6,4 +8,37 @@ class Telefone {
     required this.ddd,
     required this.numero,
   });
+
+  // Método que pega o nosso objeto Telefone e transforma em um Objeto Map<String, dynamic>
+  // toMap()
+  // Primeiro passo - Serialização
+  Map<String, dynamic> toMap() {
+    return {
+      'ddd': ddd,
+      'numero': numero,
+    };
+  }
+
+  // Método que pega um Map<String, dynamic> e transforma em um json pelo pacote dart:coverter (jsonEncode)
+  // toJson()
+  // Segundo passo - Serialização
+  String toJson() => jsonEncode(toMap());
+
+  // Recebimento de dados JSON
+  // Primeiro passo - Desserialização
+
+  factory Telefone.fromJson(String json) {
+    final jsonMap = jsonDecode(json);
+    return Telefone.fromMap(jsonMap);
+  }
+
+  // Convertendo o mapa JSON em um Objeto
+  // Segundo passo - Desserialização
+
+  factory Telefone.fromMap(Map<String, dynamic> map) {
+    return Telefone(
+      ddd: map['ddd'],
+      numero: map['numero'],
+    );
+  }
 }
